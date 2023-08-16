850f6cbf49442c078602f2e63a375a5464a91096
denmarksdev <denmarksdev@gmail.com>
2023-08-16 00:26:18 -0300

    feat: work around css loader

--- source/src/plugins/fetch-plugin.ts
+++ source/src/plugins/fetch-plugin.ts
@@ -21,18 +21,33 @@ export const fetchPlugin = (inputCode: string) => {
                 // Check to seer if we have already fetched this file
                 // and if it is n the cache
 
-                const cahcheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+                // const cahcheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-                // if it is, reutrn it immediately
-                if (cahcheResult) {
-                    return cahcheResult;
-                }
+                // // if it is, reutrn it immediately
+                // if (cahcheResult) {
+                //     return cahcheResult;
+                // }
 
                 const { data, request } = await axios.get(args.path);
 
+                const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+                const escaped = data
+                    .replace(/\n/g, '')
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "\\'")
+
+                const contents =
+                    fileType === 'css'
+                        ? `
+                    const style = document.createElement('style');
+                    style.innerText = '${escaped}';
+                    document.head.appendChild(style);
+                 `     : data;
+
                 const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
-                    contents: data,
+                    contents: contents,
                     resolveDir: new URL('./', request.responseURL).pathname
                 }
 
