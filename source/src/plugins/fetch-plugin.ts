2593a9aeff9c8b1dd98fac8336092a115ce32cfa
denmarksdev <denmarksdev@gmail.com>
2023-08-16 00:37:27 -0300

    refactor onload for css and jsx

--- source/src/plugins/fetch-plugin.ts
+++ source/src/plugins/fetch-plugin.ts
@@ -10,27 +10,24 @@ export const fetchPlugin = (inputCode: string) => {
     return {
         name: 'fetch-plugin',
         setup(build: esbuild.PluginBuild) {
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                if (args.path === 'index.js') {
-                    return {
-                        loader: 'jsx',
-                        contents: inputCode,
-                    };
-                }
+            build.onLoad({ filter: /(^index\.js)$/ }, () => {
+                return {
+                    loader: 'jsx',
+                    contents: inputCode,
+                };
+            })
 
+            build.onLoad({ filter: /.css$/ }, async (args: any) => {
                 // Check to seer if we have already fetched this file
                 // and if it is n the cache
+                const cahcheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-                // const cahcheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-
-                // // if it is, reutrn it immediately
-                // if (cahcheResult) {
-                //     return cahcheResult;
-                // }
+                // if it is, reutrn it immediately
+                if (cahcheResult) {
+                    return cahcheResult;
+                }
 
                 const { data, request } = await axios.get(args.path);
-
-                const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
 
                 const escaped = data
                     .replace(/\n/g, '')
@@ -38,16 +35,41 @@ export const fetchPlugin = (inputCode: string) => {
                     .replace(/'/g, "\\'")
 
                 const contents =
-                    fileType === 'css'
-                        ? `
+                    `
                     const style = document.createElement('style');
                     style.innerText = '${escaped}';
                     document.head.appendChild(style);
-                 `     : data;
+                 `
 
                 const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents: contents,
+                    resolveDir: new URL('./', request.responseURL).pathname
+                }
+
+                // store response cache
+                await fileCache.setItem(args.path, result);
+
+                return result;
+
+            })
+
+            build.onLoad({ filter: /.*/ }, async (args: any) => {
+                //Check to seer if we have already fetched this file
+                //and if it is n the cache
+
+                const cahcheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+                // if it is, reutrn it immediately
+                if (cahcheResult) {
+                    return cahcheResult;
+                }
+
+                const { data, request } = await axios.get(args.path);
+
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents: data,
                     resolveDir: new URL('./', request.responseURL).pathname
                 }
 
